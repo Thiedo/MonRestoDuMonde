c09b5f2250d8922dc0baacf4beaa4b415c5de95b
Marame <Marame@Mac-mini-de-Marame.local>
2018-10-05 22:14:24 +0100

    Ajout tabBar Custom

---
+++ MonRestoDuMonde/Controlleur/MonTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  MonTabBarController.swift
+//  MonRestoDuMonde
+//
+//  Created by Marame on 05/10/2018.
+//  Copyright © 2018 Gueye Gora. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIRE
+    }
+
+  
+}
---
+++ MonRestoDuMonde/Utilitaires/Constantes.swift
@@ -0,0 +1,13 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Marame on 05/10/2018.
+//  Copyright © 2018 Gueye Gora. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIRE = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
