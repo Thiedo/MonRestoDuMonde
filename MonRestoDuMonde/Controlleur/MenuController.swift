e8614063cf37d43e7d670bae8fc56f0a1d256a44
Marame <Marame@Mac-mini-de-Marame.local>
2018-10-05 22:01:26 +0100

    Ajout des controlleurs principaux

---
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Marame on 05/10/2018.
+//  Copyright © 2018 Gueye Gora. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
