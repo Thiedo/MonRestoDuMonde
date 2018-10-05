491a7d3773e63e9aacbe6591c82072579c398fc5
Marame <Marame@Mac-mini-de-Marame.local>
2018-10-05 23:28:49 +0100

    Ajout de la class Plat

---
+++ MonRestoDuMonde/Modele/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by Marame on 05/10/2018.
+//  Copyright © 2018 Gueye Gora. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    
+    case entre
+    case platPrincipal
+    case desert
+}
